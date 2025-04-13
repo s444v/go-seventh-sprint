e21e179bb19be037b9129164a901c9532ecd7d74
s444v <dymnich02@gmail.com>
2025-04-13 12:34:28 +0300

    first try

--- main_test.go
+++ main_test.go
@@ -7,16 +7,19 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+type Request struct {
+	request string
+	status  int
+	message string
+}
 
 func TestCafeNegative(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 
-	requests := []struct {
-		request string
-		status  int
-		message string
-	}{
+	requests := []Request{
 		{"/cafe", http.StatusBadRequest, "unknown city"},
 		{"/cafe?city=omsk", http.StatusBadRequest, "unknown city"},
 		{"/cafe?city=tula&count=na", http.StatusBadRequest, "incorrect count"},
@@ -26,7 +29,7 @@ func TestCafeNegative(t *testing.T) {
 		req := httptest.NewRequest("GET", v.request, nil)
 		handler.ServeHTTP(response, req)
 
-		assert.Equal(t, v.status, response.Code)
+		require.Equal(t, v.status, response.Code)
 		assert.Equal(t, v.message, strings.TrimSpace(response.Body.String()))
 	}
 }
@@ -34,17 +37,73 @@ func TestCafeNegative(t *testing.T) {
 func TestCafeWhenOk(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 
-	requests := []string{
-		"/cafe?count=2&city=moscow",
-		"/cafe?city=tula",
-		"/cafe?city=moscow&search=ложка",
+	requests := []Request{
+		{"/cafe?count=2&city=moscow", http.StatusOK, ""},
+		{"/cafe?city=tula", http.StatusOK, ""},
+		{"/cafe?city=moscow&search=ложка", http.StatusOK, ""},
 	}
 	for _, v := range requests {
 		response := httptest.NewRecorder()
-		req := httptest.NewRequest("GET", v, nil)
+		req := httptest.NewRequest("GET", v.request, nil)
 
 		handler.ServeHTTP(response, req)
 
-		assert.Equal(t, http.StatusOK, response.Code)
+		require.Equal(t, v.status, response.Code)
+	}
+}
+
+func TestCafeCountWhenOk(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+	moscowCafeList, ok := cafeList["moscow"]
+	if !ok {
+		return
+	}
+	requests := []Request{
+		{"/cafe?city=moscow&count=0", http.StatusOK, ""},
+		{"/cafe?city=moscow&count=1", http.StatusOK, strings.Join(moscowCafeList[:1], ",")},
+		{"/cafe?city=moscow&count=2", http.StatusOK, strings.Join(moscowCafeList[:2], ",")},
+		{"/cafe?city=moscow&count=101", http.StatusOK, strings.Join(moscowCafeList, ",")},
+	}
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", v.request, nil)
+		handler.ServeHTTP(response, req)
+		require.Equal(t, v.status, response.Code)
+		assert.Equal(t, v.message, strings.TrimSpace(response.Body.String()))
+	}
+}
+
+func TestCafeCountNegative(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+	requests := []Request{
+		{"/cafe?city=moscow&count=na", http.StatusBadRequest, "incorrect count"},
+		{"/cafe?city=moscow&count=blabla", http.StatusBadRequest, "incorrect count"},
+		{"/cafe?city=moscow&count=-1", http.StatusBadRequest, "incorrect count"},
+		{"/cafe?city=moscow&count=10000000000000000000000000000000000000000000000000000", http.StatusBadRequest, "incorrect count"},
+	}
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", v.request, nil)
+		handler.ServeHTTP(response, req)
+		require.Equal(t, v.status, response.Code)
+		assert.Equal(t, v.message, strings.TrimSpace(response.Body.String()))
+	}
+}
+
+func TestCafeSearch(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+	requests := []Request{
+		{"/cafe?city=moscow&search=фасоль", http.StatusOK, ""},
+		{"/cafe?city=moscow&search=кофе", http.StatusOK, "Мир кофе,Кофе и завтраки"},
+		{"/cafe?city=moscow&search=вилка", http.StatusOK, "Ложка и вилка"},
+		{"/cafe?city=moscow&search=", http.StatusOK, strings.Join(cafeList["moscow"], ",")},
+		{"/cafe?city=moscow&search=бредик", http.StatusOK, ""},
+	}
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", v.request, nil)
+		handler.ServeHTTP(response, req)
+		require.Equal(t, v.status, response.Code)
+		assert.Equal(t, v.message, strings.TrimSpace(response.Body.String()))
 	}
 }
