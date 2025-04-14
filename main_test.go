eed8d8bc295f7b2cd6dd8436c34f9016a7e789b5
s444v <dymnich02@gmail.com>
2025-04-14 09:56:26 +0300

    second try

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"strings"
 	"testing"
 
@@ -10,100 +11,121 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-type Request struct {
-	request string
-	status  int
-	message string
+type TestsTable struct {
+	request      string
+	wantStatus   int
+	wantResponse string
 }
 
 func TestCafeNegative(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 
-	requests := []Request{
+	tests := []TestsTable{
 		{"/cafe", http.StatusBadRequest, "unknown city"},
 		{"/cafe?city=omsk", http.StatusBadRequest, "unknown city"},
 		{"/cafe?city=tula&count=na", http.StatusBadRequest, "incorrect count"},
 	}
-	for _, v := range requests {
+	for _, v := range tests {
 		response := httptest.NewRecorder()
 		req := httptest.NewRequest("GET", v.request, nil)
 		handler.ServeHTTP(response, req)
 
-		require.Equal(t, v.status, response.Code)
-		assert.Equal(t, v.message, strings.TrimSpace(response.Body.String()))
+		require.Equal(t, v.wantStatus, response.Code)
+		assert.Equal(t, v.wantResponse, strings.TrimSpace(response.Body.String()))
 	}
 }
 
 func TestCafeWhenOk(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 
-	requests := []Request{
+	tests := []TestsTable{
 		{"/cafe?count=2&city=moscow", http.StatusOK, ""},
 		{"/cafe?city=tula", http.StatusOK, ""},
 		{"/cafe?city=moscow&search=ложка", http.StatusOK, ""},
 	}
-	for _, v := range requests {
+	for _, v := range tests {
 		response := httptest.NewRecorder()
 		req := httptest.NewRequest("GET", v.request, nil)
 
 		handler.ServeHTTP(response, req)
 
-		require.Equal(t, v.status, response.Code)
+		require.Equal(t, v.wantStatus, response.Code)
 	}
 }
 
 func TestCafeCountWhenOk(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
-	moscowCafeList, ok := cafeList["moscow"]
-	if !ok {
-		return
+	path := "/cafe?city=moscow&count="
+	cafe, ok := cafeList["moscow"]
+	require.True(t, ok)
+	tests := []struct {
+		count      int // передаваемое значение count
+		wantStatus int // ожидаемый статус ответа
+		wantCount  int // ожидаемое количество кафе в ответе
+	}{
+		{0, http.StatusOK, 0},
+		{1, http.StatusOK, 1},
+		{2, http.StatusOK, 2},
+		{100, http.StatusOK, min(100, len(cafe))},
 	}
-	requests := []Request{
-		{"/cafe?city=moscow&count=0", http.StatusOK, ""},
-		{"/cafe?city=moscow&count=1", http.StatusOK, strings.Join(moscowCafeList[:1], ",")},
-		{"/cafe?city=moscow&count=2", http.StatusOK, strings.Join(moscowCafeList[:2], ",")},
-		{"/cafe?city=moscow&count=101", http.StatusOK, strings.Join(moscowCafeList, ",")},
-	}
-	for _, v := range requests {
+	for _, v := range tests {
 		response := httptest.NewRecorder()
-		req := httptest.NewRequest("GET", v.request, nil)
+		req := httptest.NewRequest("GET", path+strconv.Itoa(v.count), nil)
 		handler.ServeHTTP(response, req)
-		require.Equal(t, v.status, response.Code)
-		assert.Equal(t, v.message, strings.TrimSpace(response.Body.String()))
+		require.Equal(t, v.wantStatus, response.Code)
+		if response.Body.String() == "" {
+			assert.Equal(t, v.wantCount, 0)
+		} else {
+			responseBody := strings.Split(response.Body.String(), ",")
+			assert.Equal(t, v.wantCount, len(responseBody))
+		}
 	}
 }
 
 func TestCafeCountNegative(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
-	requests := []Request{
-		{"/cafe?city=moscow&count=na", http.StatusBadRequest, "incorrect count"},
-		{"/cafe?city=moscow&count=blabla", http.StatusBadRequest, "incorrect count"},
-		{"/cafe?city=moscow&count=-1", http.StatusBadRequest, "incorrect count"},
-		{"/cafe?city=moscow&count=10000000000000000000000000000000000000000000000000000", http.StatusBadRequest, "incorrect count"},
+	path := "/cafe?city=moscow&count="
+	tests := []struct {
+		count        string // передаваемое значение count
+		wantStatus   int    // ожидаемый статус ответа
+		wantResponse string // ожидаемое в ответе
+	}{
+		{"na", http.StatusBadRequest, "incorrect count"},
+		{"-1", http.StatusBadRequest, "incorrect count"},
+		{"10000000000000000000000000000000000000000000000000000", http.StatusBadRequest, "incorrect count"},
 	}
-	for _, v := range requests {
+	for _, v := range tests {
 		response := httptest.NewRecorder()
-		req := httptest.NewRequest("GET", v.request, nil)
+		req := httptest.NewRequest("GET", path+v.count, nil)
 		handler.ServeHTTP(response, req)
-		require.Equal(t, v.status, response.Code)
-		assert.Equal(t, v.message, strings.TrimSpace(response.Body.String()))
+		require.Equal(t, v.wantStatus, response.Code)
+		assert.Equal(t, v.wantResponse, strings.TrimSpace(response.Body.String()))
 	}
 }
 
 func TestCafeSearch(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
-	requests := []Request{
-		{"/cafe?city=moscow&search=фасоль", http.StatusOK, ""},
-		{"/cafe?city=moscow&search=кофе", http.StatusOK, "Мир кофе,Кофе и завтраки"},
-		{"/cafe?city=moscow&search=вилка", http.StatusOK, "Ложка и вилка"},
-		{"/cafe?city=moscow&search=", http.StatusOK, strings.Join(cafeList["moscow"], ",")},
-		{"/cafe?city=moscow&search=бредик", http.StatusOK, ""},
+	path := "/cafe?city=moscow&search="
+	tests := []struct {
+		search     string // передаваемое значение search
+		wantStatus int    // ожидаемый статус ответа
+		wantCount  int    // ожидаемое количество кафе в ответе
+	}{
+		{"фасоль", http.StatusOK, 0},
+		{"кофе", http.StatusOK, 2},
+		{"вилка", http.StatusOK, 1},
+		{"", http.StatusOK, 5},
 	}
-	for _, v := range requests {
+	for _, v := range tests {
 		response := httptest.NewRecorder()
-		req := httptest.NewRequest("GET", v.request, nil)
+		req := httptest.NewRequest("GET", path+v.search, nil)
 		handler.ServeHTTP(response, req)
-		require.Equal(t, v.status, response.Code)
-		assert.Equal(t, v.message, strings.TrimSpace(response.Body.String()))
+		require.Equal(t, v.wantStatus, response.Code)
+		if response.Body.String() == "" {
+			assert.Equal(t, v.wantCount, 0)
+		} else {
+			responseBody := strings.Split(response.Body.String(), ",")
+			assert.Equal(t, v.wantCount, len(responseBody))
+		}
 	}
 }
